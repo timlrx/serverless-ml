23bd9f06bfd94aefa6afba920d2a2fe659d0adda
Lenovo\timothy <timothy0336@hotmail.com>
2020-01-22 00:36:39 +0800

    Add routes for tweet.R

--- twitter-r/app.R
+++ twitter-r/app.R
@@ -1,5 +1,5 @@
-  
-# app.R
+library(plotly)
+source("tweet.R")
 
 #* CORS allows requests made from external urls to be accepted
 #* @filter cors
@@ -14,28 +14,51 @@ function(req, res){
   res$status <- 200
 }
 
-
-#* Echo back the input
-#* @param msg The message to echo
-#* @get /echo
-function(msg=""){
-  list(msg = paste0("The message is: '", msg, "'"))
-}
-
-#* Plot a histogram
+#* Plot tweet frequency over time
+#* @param users Comma seperated list of user name or IDs
+#* @param n Number of tweets (max 3200)
 #* @png
-#* @get /plot
-function(){
-  rand <- rnorm(100)
-  hist(rand)
+#* @get /frequency
+function(users="", n=400){
+  users_vector <- strsplit(users, ",")[[1]]
+  token <- authenticate()
+  tmls <- get_tweets(users_vector, min(3200, as.numeric(n)), token)
+  plot_tweet_trends(tmls) %>% print()
 }
 
-#* Return the sum of two numbers
-#* @param a The first number to add
-#* @param b The second number to add
-#* @get /sum
-search <- function(a="1", b="1"){
-  as.numeric(a) + as.numeric(b)
+#* Plot tweet frequency over time (interactive)
+#* @param users Comma seperated list of user name or IDs
+#* @param n Number of tweets (max 3200)
+#* @serializer htmlwidget
+#* @get /html/frequency
+function(users="", n=400){
+  users_vector <- strsplit(users, ",")[[1]]
+  token <- authenticate()
+  tmls <- get_tweets(users_vector, min(3200, as.numeric(n)), token)
+  plot_tweet_trends(tmls) %>% ggplotly()
 }
 
-print('app.R running')
+
+#* Plot sentiment trends over time
+#* @param users Comma seperated list of user name or IDs
+#* @param n Number of tweets (max 3200)
+#* @png
+#* @get /sentiment
+function(users="", n=400){
+  users_vector <- strsplit(users, ",")[[1]]
+  token <- authenticate()
+  tmls <- get_tweets(users_vector, min(3200, as.numeric(n)), token)
+  plot_sentiment_trends(tmls) %>% print()
+}
+
+#* Plot sentiment trends over time (interactive)
+#* @param users Comma seperated list of user name or IDs
+#* @param n Number of tweets (max 3200)
+#* @serializer htmlwidget
+#* @get /html/sentiment
+function(users="", n=400){
+  users_vector <- strsplit(users, ",")[[1]]
+  token <- authenticate()
+  tmls <- get_tweets(users_vector, min(3200, as.numeric(n)), token)
+  plot_sentiment_trends(tmls) %>% ggplotly()
+}
